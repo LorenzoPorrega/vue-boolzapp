94be1369caf4ccda4bfc42963caaf30b0a26ddb0
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-08-18 17:01:37 +0200

    milestone 5 date - edit on message in WIP

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ const app = createApp({
           avatar: "_1",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Hai portato a spasso il cane?",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Ricordati di dargli da mangiare",
               status: "sent",
             },
             {
-              date: "10/01/2020 16:15:22",
+              date: "10/01/2020 16:15",
               message: "Tutto fatto!",
               status: "received",
             },
@@ -30,17 +30,17 @@ const app = createApp({
           avatar: "_2",
           messages: [
             {
-              date: "20/03/2020 16:30:00",
+              date: "20/03/2020 16:30",
               message: "Ciao come stai?",
               status: "sent",
             },
             {
-              date: "20/03/2020 16:30:55",
+              date: "20/03/2020 16:30",
               message: "Bene grazie! Stasera ci vediamo?",
               status: "received",
             },
             {
-              date: "20/03/2020 16:35:00",
+              date: "20/03/2020 16:35",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "received",
             },
@@ -51,17 +51,17 @@ const app = createApp({
           avatar: "_3",
           messages: [
             {
-              date: "28/03/2020 10:10:40",
+              date: "28/03/2020 10:10",
               message: "La Marianna va in campagna",
               status: "received",
             },
             {
-              date: "28/03/2020 10:20:10",
+              date: "28/03/2020 10:20",
               message: "Sicuro di non aver sbagliato chat?",
               status: "sent",
             },
             {
-              date: "28/03/2020 16:15:22",
+              date: "28/03/2020 16:15",
               message: "Ah scusa!",
               status: "received",
             },
@@ -72,12 +72,12 @@ const app = createApp({
           avatar: "_4",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "1vs1 CS:Go? 🤡",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Zì, badrone...",
               status: "received",
             },
@@ -88,12 +88,12 @@ const app = createApp({
           avatar: "_5",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Eccoci qua 🤡",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "EEEEEEEEEEEEEEEEEEEEEASY",
               status: "received",
             },
@@ -104,12 +104,12 @@ const app = createApp({
           avatar: "_6",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Ngopp a mammt",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Eccoci qua",
               status: "received",
             },
@@ -120,12 +120,12 @@ const app = createApp({
           avatar: "_7",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Lo sai che hanno pubblicato una nuova versione della distro di Arch?",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Véro dici? Comunque io uso solo Ubuntu..",
               status: "received",
             },
@@ -136,12 +136,12 @@ const app = createApp({
           avatar: "_8",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Margot Robbie o Megan Fox?",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Megan Fox, indiscutibilmente.",
               status: "received",
             },
@@ -152,12 +152,12 @@ const app = createApp({
           avatar: "_2",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Bella Simon",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "BEEEELLLLAAAAAAAAAA",
               status: "received",
             },
@@ -168,22 +168,22 @@ const app = createApp({
           avatar: "_6",
           messages: [
             {
-              date: "10/01/2020 15:30:55",
+              date: "10/01/2020 15:30",
               message: "Bella uaiò",
               status: "received",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Eccochi qua",
               status: "sent",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Haram o sessione su Genshin Impact?",
               status: "received",
             },
             {
-              date: "10/01/2020 15:50:00",
+              date: "10/01/2020 15:50",
               message: "Mah, io mi sparerei una Warzonata EAZY con gli altri tbh",
               status: "sent",
             },
@@ -205,28 +205,41 @@ const app = createApp({
     },
     //method to take message input text via const newMessage, to push the message in corresponding chat and to then clear the text input
     sentMessage(){
-      const newMessage = this.text;
+      const newMessage = this.text.trim();
       this.text = "";
-      setTimeout(() =>{
-        this.contacts[this.currentContactChat].messages.push(
-          {
-            date: "10/01/2020 15:50:00",
-            message: newMessage,
-            status: "sent",
-          }
-        );
-        setTimeout(() => {
+
+      if(newMessage === "" || newMessage === " "){
+        return
+      }
+      else{
+        setTimeout(() =>{
           this.contacts[this.currentContactChat].messages.push(
             {
-              date: "10/01/2020 15:50:00",
-              message: "Ok",
-              status: "received",
+              date: (new Intl.DateTimeFormat("it", {
+                dateStyle: "short",
+                timeStyle: "short",
+              }).format(new Date())).replace(",", ""),
+              message: newMessage,
+              status: "sent",
             }
           );
+          setTimeout(() => {
+            this.contacts[this.currentContactChat].messages.push(
+              {
+                date: (new Intl.DateTimeFormat("it", {
+                  dateStyle: "short",
+                  timeStyle: "short",
+                }).format(new Date())).replace(",", ""),
+                message: "Ok",
+                status: "received",
+              }
+            );
+          },
+          500 + (Math.random() * 5000));
         },
-        1000);
-      },
-      100);
+        100);
+      }
+    
     },
     sender(i, contacts){
       //Method to print who sent the message ("Tu" = You) based on the last message's class ("sent" or "received")
